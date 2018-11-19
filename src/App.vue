cdbadc80b957db9627bca55a9d6583368985120d
amsa2diop <varafd@gmail.com>
2018-11-19 19:12:46 -0200

    AGORA VAI

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,10 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+  <v-app >
+    <div class="red darken-4">
+      <v-content>
+        <HelloWorld/>  
+      </v-content>
+    </div>
   </v-app>
 </template>
 
